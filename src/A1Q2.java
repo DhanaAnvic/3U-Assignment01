40dc81b52984207b278f8443fc0b7603942d94ef
DhanaAnvic <fabed2976@googleapps.wrdsb.ca>
2016-09-29 09:20:05 -0400

    Update A1Q2.java

--- src/A1Q2.java
+++ src/A1Q2.java
@@ -1,6 +1,9 @@
 
+
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -22,11 +25,39 @@ public class A1Q2 {
         
         //A1Q2
         City SK = new City();
+        Robot Chen = new Robot (SK, 7,7, Direction. WEST);
         
         //create Walls
         new Wall (SK, 7,7, Direction. NORTH);
         new Wall (SK, 7,6, Direction. NORTH);
         new Wall (SK, 7,6, Direction. WEST);
-                
+        new Wall (SK, 8,6, Direction. WEST);
+        new Wall (SK, 8,6, Direction. SOUTH);
+        new Wall (SK, 7,7, Direction. SOUTH);
+        new Wall (SK, 7,7, Direction. EAST);
+        new Thing (SK, 8,7);
+        
+        //Get Chen to move
+        Chen. move ();
+        Chen. turnLeft ();
+        Chen. move ();
+        Chen. turnLeft ();
+        Chen. move ();
+        Chen. pickThing ();
+        Chen. turnLeft();
+        Chen. turnLeft();
+        Chen. move ();
+        Chen. turnLeft ();
+        Chen. turnLeft ();
+        Chen. turnLeft();
+        Chen. move ();
+        Chen. turnLeft ();
+        Chen. turnLeft ();
+        Chen. turnLeft ();
+        Chen. move ();
+        Chen. turnLeft ();
+        Chen. turnLeft ();
+        
+        
     }
 }
